22a6dd78bb0e051f42488d2c252cf22f491f536a
PeterKams <peter.kamlewechi@bison.howard.edu>
2018-10-18 18:19:24 -0400

    Calculated and printed number 0 and 2 in the Id

--- CompOrg- Project 1.s
+++ CompOrg- Project 1.s
@@ -9,5 +9,14 @@ main:	#Code starts off here
 	li $v0, 11
 	syscall		#prints @ 
 
+	
+	li $v0, 1	#updates to print using the integer system call
+	addi $a0, $a0, -64 
+	syscall		#0 is printed
+
+	addi $a0, $a0, 2 #2 is calculated
+	syscall		#2 is printed
+
+
 
 	jr $ra 		#return statement
