1d8b9a435067a6a641c106f2ce8ac1d652426d92
PeterKams <peter.kamlewechi@bison.howard.edu>
2018-10-18 18:07:17 -0400

    Added and printed the @ sign in student ID

--- CompOrg- Project 1.s
+++ CompOrg- Project 1.s
@@ -1,10 +1,13 @@
 .data	# Data declaration section
+at_output:	.word 64 #@ address is passed here
 
-.text	#Aseembly language instruction
+.text			#Aseembly language instruction
 .globl main
 
 main:	#Code starts off here
+	lw $a0, at_output
+	li $v0, 11
+	syscall		#prints @ 
 
 
-
-	jr $ra #return statement
+	jr $ra 		#return statement
