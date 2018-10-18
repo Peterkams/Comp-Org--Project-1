458672ff40141661b76d0580c6416c8cc484f7d1
PeterKams <peter.kamlewechi@bison.howard.edu>
2018-10-18 16:52:32 -0400

    Updated project file by adding some sections to the file

--- CompOrg- Project 1.s
+++ CompOrg- Project 1.s
@@ -1,5 +1,10 @@
-	.text
-	.globl main
-main: 		#code's main function
-	
+.data	# Data declaration section
+
+.text	#Aseembly language instruction
+.globl main
+
+main:	#Code starts off here
+
+
+
 	jr $ra #return statement
